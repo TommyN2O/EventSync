37a30c8981a3a5b79c926634142436b6823df738
TommyN2O <tomas.nester@stud.vilniustech.lt>
2025-07-03 14:48:10 +0300

    first commit

---
+++ src/main/java/com/eventsync/demo/EventSyncApplication.java
@@ -0,0 +1,13 @@
+package com.eventsync.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSyncApplication.class, args);
+	}
+
+}
